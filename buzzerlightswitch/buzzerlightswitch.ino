44226f2f9694362350a6675c16f4e951b898ac44
j842 <j@jde.nz>
2018-06-30 20:19:07 +1200

    Now with joystick to control servo

--- buzzerlightswitch/buzzerlightswitch.ino
+++ buzzerlightswitch/buzzerlightswitch.ino
@@ -7,11 +7,44 @@
 #define BUTTON2 12
 #define BUTTON3 22
 
+#define JOYSWITCH 2
+#define JOYX 0
+#define JOYY 1
+
+
 #include "pitches.h"
 #include "xmas.h"
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+class servo 
+{
+  public:
+    servo(int pin) : mPin(pin), mAngle(0) {}
+    setup() {   
+        mServo.attach(mPin);  // attaches the servo on pin 9 to the servo object
+        mServo.write(mAngle);
+    }
+
+    void rotateby(int delta)
+    {
+      mAngle += delta;
+      if (mAngle>180) mAngle=180;
+      if (mAngle<0) mAngle=0;
+      mServo.write(mAngle);
+    }
+
+    void rotateto(int a)
+    {
+      mAngle = a;
+      rotateby(0);
+    }
+    
+    
+    int mPin;
+    int mAngle;
+    Servo mServo;  // create servo object to control a servo
+};
+
 int pos = 0;    // variable to store the servo position
 
 class button 
@@ -50,6 +83,7 @@ void setled(int red, int green, int blue)
 button button1(BUTTON1);
 button button2(BUTTON2);
 button button3(BUTTON3);
+servo servo1(SERVO);
 
 
 void setup() {
@@ -58,6 +92,8 @@ void setup() {
   button2.setup();
   button3.setup();
 
+  servo1.setup();
+
   pinMode(RED, OUTPUT);
   pinMode(GREEN,OUTPUT); 
   pinMode(BLUE,OUTPUT);
@@ -65,11 +101,20 @@ void setup() {
 
   pinMode(13,OUTPUT);
 
-  myservo.attach(SERVO);  // attaches the servo on pin 9 to the servo object
+  pinMode(JOYSWITCH, INPUT);
+  digitalWrite(JOYSWITCH, HIGH);
 }
 
 void loop() {
   static int phase = 0;
+
+  // joystick
+  int joyx = analogRead(JOYX);
+  int joyy = analogRead(JOYY);
+  if (joyx<100 || joyy<100)
+    { servo1.rotateby(-10); delay(20); }
+  if (joyx>924 || joyy>924)
+    { servo1.rotateby(10); delay(20); }
 
   if (button1.pressed())
       xmas(phase);
@@ -85,7 +130,7 @@ void loop() {
             setled(255,0,0);
             tone(8, NOTE_C5, 200);
             phase = 1;
-            myservo.write(0);
+            //servo1.rotateto(0);
             delay(200);
             break;
   
@@ -93,7 +138,7 @@ void loop() {
             setled(0,255,0);
             tone(8, NOTE_D5, 200);
             phase = 2;
-            myservo.write(90);
+            //servo1.rotateto(90);
             delay(200);
             break;        
   
@@ -101,7 +146,7 @@ void loop() {
             setled(0,0,255);
             tone(8, NOTE_E5, 200);
             phase = 0;
-            myservo.write(180);
+            //servo1.rotateto(180);
             delay(200);
             break;       
       }
