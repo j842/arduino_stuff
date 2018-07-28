c043e5b58139c4d6cae576262770b8c234ae5f1b
John <j842@users.noreply.github.com>
2018-07-28 18:41:31 +1200

    static text

--- tommy_controlpanel_firealarm/tommy_controlpanel_firealarm.ino
+++ tommy_controlpanel_firealarm/tommy_controlpanel_firealarm.ino
@@ -41,6 +41,9 @@ jpot pot1(A0);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(57600);
+
+    mx.begin();
+
   
   button_alarm.setup();
   buzzer1.setup();
@@ -104,8 +107,76 @@ class jtimer
 
     void show(int val)
     {
-      
+      String s(val);
+      printText(0,3,s.c_str());
     }
+
+    private:
+
+    #define CHAR_SPACING  1 // pixels between characters
+
+
+    void printText(uint8_t modStart, uint8_t modEnd, const char *pMsg)
+// Print the text string to the LED matrix modules specified.
+// Message area is padded with blank columns after printing.
+{
+  uint8_t   state = 0;
+  uint8_t   curLen;
+  uint16_t  showLen;
+  uint8_t   cBuf[8];
+  int16_t   col = ((modEnd + 1) * COL_SIZE) - 1;
+
+  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
+
+  do     // finite state machine to print the characters in the space available
+  {
+    switch(state)
+    {
+      case 0: // Load the next character from the font table
+        // if we reached end of message, reset the message pointer
+        if (*pMsg == '\0')
+        {
+          showLen = col - (modEnd * COL_SIZE);  // padding characters
+          state = 2;
+          break;
+        }
+
+        // retrieve the next character form the font file
+        showLen = mx.getChar(*pMsg++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
+        curLen = 0;
+        state++;
+        // !! deliberately fall through to next state to start displaying
+
+      case 1: // display the next part of the character
+        mx.setColumn(col--, cBuf[curLen++]);
+
+        // done with font character, now display the space between chars
+        if (curLen == showLen)
+        {
+          showLen = CHAR_SPACING;
+          state = 2;
+        }
+        break;
+
+      case 2: // initialize state for displaying empty columns
+        curLen = 0;
+        state++;
+        // fall through
+
+      case 3:  // display inter-character spacing or end of message padding (blank columns)
+        mx.setColumn(col--, 0);
+        curLen++;
+        if (curLen == showLen)
+          state = 0;
+        break;
+
+      default:
+        col = -1;   // this definitely ends the do loop
+    }
+  } while (col >= (modStart * COL_SIZE));
+
+  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
+}
 
     bool mRunning;
     bool mPlaying;
