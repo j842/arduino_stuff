798b089b9574ca75cf5870b2070eb31279de2db9
John <j842@users.noreply.github.com>
2020-10-04 19:50:07 +1300

    Update main.cpp
    red blue

--- lolinnodemcuv3/lolintest/LolinTest/src/main.cpp
+++ lolinnodemcuv3/lolintest/LolinTest/src/main.cpp
@@ -22,41 +22,47 @@ char ReceivedMessage[255];  // buffer for incoming packets
 // based on guide here:
 // https://medium.com/@loginov_rocks/quick-start-with-nodemcu-v3-esp8266-arduino-ecosystem-and-platformio-ide-b8415bf9a038
 
-//#define LED D0
-#define LED LED_BUILTIN
+#define LED_RED D1
+#define LED_BLUE D0
 
 
-
-void ledOn()
-{
-  digitalWrite(LED, LOW);
-}
-void ledOff()
+void ledOn(uint8_t LED)
 {
   digitalWrite(LED, HIGH);
+}
+void ledOff(uint8_t LED)
+{
+  digitalWrite(LED, LOW);
 }
 
 
 void setup()
 { // connect via wifi to set up credentials (temporarily creates an access point to connect to)
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
+  ledOn(LED_RED);
+
   Serial.begin(115200);
   WiFiManager wifiManager;
   wifiManager.autoConnect("NodeMCU Setup");
   Serial.println("WiFi Connected!");
 
-  pinMode(LED, OUTPUT);
-
   int rval = Udp.begin(localUdpPort);
   if (rval==1)
   {
     Serial.printf("I am listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
-    ledOn();
   }
   else
   {
     Serial.printf("Failed to open UDP port %d\n",localUdpPort);
-    ledOff();
+    ledOn(LED_RED);
+    exit(-1);
   }
+
+  ledOn(LED_BLUE);
+  delay(1000);
+  ledOff(LED_RED);
 }
 
 
@@ -66,7 +72,8 @@ void loop()
 
   if (packetSize>0)
   {
-    ledOff();
+    ledOff(LED_BLUE);
+    ledOff(LED_RED);
     // receive incoming UDP message
     Serial.printf("Received %d message from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(ReceivedMessage, 255);
@@ -78,13 +85,17 @@ void loop()
     }
     Serial.printf("UDP message: %s\n", ReceivedMessage);
 
+    if (tolower(ReceivedMessage[0])=='r')
+      ledOn(LED_RED);
+    else if (tolower(ReceivedMessage[0]=='b'))
+      ledOn(LED_BLUE);
+
     // send back a reply, to the IP address and port we got the packet from
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     std::string response;
     response="Recieved: \""+std::string(ReceivedMessage)+"\"\n";
     Udp.write(response.c_str());
     Udp.endPacket();
-    ledOn();
   }
 
 }
